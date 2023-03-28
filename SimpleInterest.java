2682e328b047b18d604efde75d900401582b4d5d
Raunak Shrestha <119580096+raunak-sh@users.noreply.github.com>
2023-03-28 21:00:40 +0545

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,10 +1,11 @@
-	import java.awt.FlowLayout;
-	import java.awt.event.ActionEvent;
-	import java.awt.event.ActionListener;
-	import javax.swing.JButton;
-	import javax.swing.JFrame;
-	import javax.swing.JLabel;
-	import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 
 
 	public class SimpleInterest extends JFrame {
